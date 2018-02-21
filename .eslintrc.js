b0812b0231b4c9ad548aee60aeff3bd725bb2f0e
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-21 09:58:09 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+    "react/jsx-filename-extension": false,
+    "react/prefer-stateless-function": false,
+
+  }
+};
